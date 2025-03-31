5eae1be96f7987cd2b1545af16d041f33864f8cf
denisdiaconu <denisdiaconu117@yahoo.com>
2025-03-31 11:33:13 +0200

    Add constant and links

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className='bg-pink-700 transition-all duration-200 h-[12vh] z-[100] fixed w-full'>
+      <div className="flex items-center h-full justify-between w-[90%] sm:w-[80%] mx-auto">
+        <div className='text-white font-bold text-2xl sm:text-3xl'>LOGO</div>
+      </div>
+    </div>
   )
 }
 
