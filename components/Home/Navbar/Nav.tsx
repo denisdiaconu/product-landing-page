8f47040fdbdd2fdd25633e9207e666a9b7d85fab
denisdiaconu <denisdiaconu117@yahoo.com>
2025-04-01 11:25:55 +0200

    Add buy now button to nav

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,6 +1,7 @@
 import { navLinks } from '@/constant/constant';
 import Link from 'next/link';
 import React from 'react';
+import { FaShoppingBag } from 'react-icons/fa';
 
 const Nav = () => {
   return (
@@ -19,6 +20,19 @@ const Nav = () => {
               </Link>
             );
           })}
+        </div>
+        <div className="flex items-center space-x-4">
+          <a
+            href="#_"
+            className="box-border relative z-30 inline-flex items-center justify-center w-auto px-5 py-2.5 overflow-hidden font-bold text-white transition-all duration-300 bg-green-500 rounded-md cursor-pointer group ring-offset-2 ring-1 ring-green-300 ring-offset-green-200 hover:ring-offset-green-500 ease focus:outline-none"
+          >
+            <span className="absolute bottom-0 right-0 w-8 h-20 -mb-8 -mr-5 transition-all duration-300 ease-out transform rotate-45 translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
+            <span className="absolute top-0 left-0 w-20 h-8 -mt-1 -ml-12 transition-all duration-300 ease-out transform -rotate-45 -translate-x-1 bg-white opacity-10 group-hover:translate-x-0"></span>
+            <span className="relative z-20 flex items-center text-sm">
+              <FaShoppingBag className='mr-3 w-5 h-5'/>
+              Buy Now
+            </span>
+          </a>
         </div>
       </div>
     </div>
