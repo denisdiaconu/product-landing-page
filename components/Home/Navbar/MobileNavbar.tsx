6a253a0746f05ab6db89aa6bda7991be36305deb
denisdiaconu <denisdiaconu117@yahoo.com>
2025-04-02 11:20:43 +0200

    Add mobile menu

--- components/Home/Navbar/MobileNavbar.tsx
+++ components/Home/Navbar/MobileNavbar.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { navLinks } from '@/constant/constant';
+import Link from 'next/link';
+import React from 'react';
 
 const MobileNavbar = () => {
   return (
-    <div>MobileNavbar</div>
-  )
-}
+    <div>
+      <div className="bg-black fixed inset-0 z-[1002] transform transition-all duration-500 opacity-70 w-full h-screen"></div>
+      <div
+        className="text-white bg-pink-600 w-[80%] sm:w-[60%] fixed justify-center flex flex-col h-full transform transition-all duration-500 delay-300
+        space-y-6 z-[1050]"
+      >
+        {navLinks.map((link) => {
+          return (
+            <Link key={link.id} href={link.url}>
+              <p className="text-white w-fit text-xl sm:text-[30px] ml-12 pb-1 border-b-[1.5px] border-white">
+                {link.label}
+              </p>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default MobileNavbar
+export default MobileNavbar;
