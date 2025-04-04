de25178972ffcbbb9a7e844264e0f04c869c7684
denisdiaconu <denisdiaconu117@yahoo.com>
2025-04-04 11:55:32 +0200

    Add heading to hero

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Hero = () => {
-  return <div className='bg-pink-800 flex flex-col justify-center relative w-full h-screen bg_clip'>Hero</div>;
+  return (
+    <div className="bg-pink-800 flex flex-col justify-center relative w-full h-screen bg_clip">
+      <div className="w-[90%] md:w-[80%] mx-auto items-center grid grid-cols-1 xl:grid-cols-2 gap-10">
+        <div>
+          <h1 className="text-white font-bold text-3xl md:text-4xl lg:text-5xl mt-6 mb-6 leading-[2.5rem] md:leading-[3.5rem]">
+            Superior sound, tangible quality
+          </h1>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
