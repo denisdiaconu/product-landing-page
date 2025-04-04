e92f893b099d6853d339f45e3bb8445509afa051
denisdiaconu <denisdiaconu117@yahoo.com>
2025-04-04 11:28:48 +0200

    Add hero component

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Hero from './Hero';
 
 const Home = () => {
-  return <div className='overflow-hidden'>Home</div>;
+  return <div className='overflow-hidden'>
+    <Hero />
+  </div>;
 };
 
 export default Home;
