1392a6bf73567b32e0162e75c5ab2ace3ae8f0c9
denisdiaconu <denisdiaconu117@yahoo.com>
2025-04-04 11:58:40 +0200

    Add description to hero

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -8,6 +8,11 @@ const Hero = () => {
           <h1 className="text-white font-bold text-3xl md:text-4xl lg:text-5xl mt-6 mb-6 leading-[2.5rem] md:leading-[3.5rem]">
             Superior sound, tangible quality
           </h1>
+          <p className="text-gray-300 text-xs sm:text-sm md:text-base font-medium">
+            Sit back, relax and prepare to experience a new level of audio
+            excellence. These headphones combine cutting-edge technology with
+            the finest craftsmanship, for the ultimate listening experience.
+          </p>
         </div>
         <div></div>
       </div>
