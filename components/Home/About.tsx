3828ddf2ea770849e2f3c40fed737a9db4cb2bc3
denisdiaconu <denisdiaconu117@yahoo.com>
2025-04-09 09:29:33 +0200

    Add about in home

--- components/Home/About.tsx
+++ components/Home/About.tsx
@@ -8,7 +8,13 @@ type Props = {
 };
 
 const About = ({ imageOrder, textOrder, title, linkText }: Props) => {
-  return <div>About</div>;
+  return <div className='pt-16 pb-16'>
+    <div className='w-[80%] mx-auto grid items-center grid-cols-1 lg:grid-cols-2 gap-12'>
+        <div className={`${textOrder}`}>
+            <h1 className='font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl'>{title}</h1>
+        </div>
+    </div>
+  </div>;
 };
 
 export default About;
