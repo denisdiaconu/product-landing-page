34fe5fa893e4597190abd5cb7f0cb9fc0bd305e3
denisdiaconu <denisdiaconu117@yahoo.com>
2025-04-10 11:02:21 +0200

    Add title to review

--- components/Home/Review/Review.tsx
+++ components/Home/Review/Review.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Review = () => {
   return (
-    <div>Review</div>
+    <div className='pt-16 pb-16'>
+        <h1 className='text-gray-800 dark:text-white text-2xl sm:text-3xl text-center font-bold'>Trusted by 10,000+ Happy Customers</h1>
+        <span className='bg-pink-600 w-16 h-1 mx-auto mt-3 block'></span>
+    </div>
   )
 }
 
