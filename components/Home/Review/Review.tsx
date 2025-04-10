4b02e45bc5a9f9877df6b665d563c59f84e417df
denisdiaconu <denisdiaconu117@yahoo.com>
2025-04-10 11:08:00 +0200

    Add react-multi-carousel package

--- components/Home/Review/Review.tsx
+++ components/Home/Review/Review.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReviewSlider from './ReviewSlider'
 
 const Review = () => {
   return (
@@ -6,7 +7,7 @@ const Review = () => {
         <h1 className='text-gray-800 dark:text-white text-2xl sm:text-3xl text-center font-bold'>Trusted by 10,000+ Happy Customers</h1>
         <span className='bg-pink-600 w-16 h-1 mx-auto mt-3 block'></span>
         <div className='w-[90%] sm:w-[80%] lg:w-[60%] mx-auto mt-16'>
-            
+            <ReviewSlider />
         </div>
     </div>
   )
