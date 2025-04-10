8f0dc37b2431a81ab4991de7037fe8917ed0936c
denisdiaconu <denisdiaconu117@yahoo.com>
2025-04-10 11:06:03 +0200

    Add review slider component

--- components/Home/Review/Review.tsx
+++ components/Home/Review/Review.tsx
@@ -5,6 +5,9 @@ const Review = () => {
     <div className='pt-16 pb-16'>
         <h1 className='text-gray-800 dark:text-white text-2xl sm:text-3xl text-center font-bold'>Trusted by 10,000+ Happy Customers</h1>
         <span className='bg-pink-600 w-16 h-1 mx-auto mt-3 block'></span>
+        <div className='w-[90%] sm:w-[80%] lg:w-[60%] mx-auto mt-16'>
+            
+        </div>
     </div>
   )
 }
