a7b7d9a2644d29ff164f2962a611ab693720e0e5
denisdiaconu <denisdiaconu117@yahoo.com>
2025-04-11 10:41:50 +0200

    Add carousel info

--- components/Home/Review/Review.tsx
+++ components/Home/Review/Review.tsx
@@ -3,14 +3,16 @@ import ReviewSlider from './ReviewSlider';
 
 const Review = () => {
   return (
-    <div className='pt-16 pb-16'>
-        <h1 className='text-gray-800 dark:text-white text-2xl sm:text-3xl text-center font-bold'>Trusted by 10,000+ Happy Customers</h1>
-        <span className='bg-pink-600 w-16 h-1 mx-auto mt-3 block'></span>
-        <div className='w-[90%] sm:w-[80%] lg:w-[60%] mx-auto mt-16'>
-            <ReviewSlider />
-        </div>
+    <div className="pt-16 pb-16">
+      <h1 className="text-gray-800 dark:text-white text-2xl sm:text-3xl text-center font-bold">
+        Trusted by 10,000+ Happy Customers
+      </h1>
+      <span className="bg-pink-600 w-16 h-1 mx-auto mt-3 block"></span>
+      <div className="w-[90%] sm:w-[80%] lg:w-[60%] mx-auto mt-16">
+        <ReviewSlider />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Review
+export default Review;
