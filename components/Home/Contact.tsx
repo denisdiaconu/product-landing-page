196b30516c0abef5e675b8e5ca7f8bc78d93e21a
denisdiaconu <denisdiaconu117@yahoo.com>
2025-04-14 11:20:48 +0200

    Add content to contact us

--- components/Home/Contact.tsx
+++ components/Home/Contact.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="pt-16 pb-16">
+      <h1 className="text-gray-800 dark:text-white text-2xl sm:text-3xl text-center font-bold">
+        Get In Touch
+      </h1>
+      <span className="bg-pink-600 block w-16 h-1 mx-auto mt-3"></span>
+      <p className="text-gray-700 dark:text-gray-200 mt-4 text-center text-sm sm:text-base">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+      </p>
+      <p className="text-gray-700 dark:text-gray-200 text-center text-sm sm:text-base">
+        Consequatur pariatur molestias odio.
+      </p>
+    </div>
+  );
+};
 
-export default Contact
+export default Contact;
