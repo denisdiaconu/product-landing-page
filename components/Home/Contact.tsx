db7db2564f829166976a172711a7d473b1eeb4c1
denisdiaconu <denisdiaconu117@yahoo.com>
2025-04-14 11:48:15 +0200

    Add inputs to contact

--- components/Home/Contact.tsx
+++ components/Home/Contact.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaPaperPlane } from 'react-icons/fa';
 
 const Contact = () => {
   return (
@@ -13,6 +14,31 @@ const Contact = () => {
       <p className="text-gray-700 dark:text-gray-200 text-center text-sm sm:text-base">
         Consequatur pariatur molestias odio.
       </p>
+      <div className="w-[90%] sm:w-[80%] lg:w-[60%] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-6 items-center mt-16">
+        <div className="h-full">
+          <input
+            type="text"
+            placeholder="Name"
+            className="bg-gray-200 text-black px-6 py-3.5 mb-4 w-full block shadow-md outline-none rounded-md"
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            className="bg-gray-200 text-black px-6 py-3.5 mb-4 w-full block shadow-md outline-none rounded-md"
+          />
+          <input
+            type="text"
+            placeholder="Subject"
+            className="bg-gray-200 text-black px-6 py-3.5 w-full block shadow-md outline-none rounded-md"
+          />
+        </div>
+        <div className='h-full'>
+            <textarea placeholder='Message' className="bg-gray-200 text-black px-6 py-3.5 w-full h-full block shadow-md outline-none rounded-md mb-4"></textarea>
+        </div>
+      </div>
+      <button className='bg-pink-800 text-white mt-8 flex items-center gap-2 px-6 py-4 rounded-md shadow-md hover:bg-pink-900 transition mx-auto cursor-pointer'>
+        <FaPaperPlane size={18}/> Send Message
+      </button>
     </div>
   );
 };
