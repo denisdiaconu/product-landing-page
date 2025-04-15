04e6630665a9e79fabdd9c7c2788cb145eebb7fc
denisdiaconu <denisdiaconu117@yahoo.com>
2025-04-15 11:38:23 +0200

    Add logo to footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Poppins } from 'next/font/google';
 import './globals.css';
 import ResponsiveNav from '@/components/Home/Navbar/ResponsiveNav';
 import Provider from '@/components/Hoc/Provider';
+import Footer from '@/components/Home/Footer';
 
 const font = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -25,6 +26,7 @@ export default function RootLayout({
         <Provider>
           <ResponsiveNav />
           {children}
+          <Footer />
         </Provider>
       </body>
     </html>
--- components/Home/Footer.tsx
+++ components/Home/Footer.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="bg-gray-950 pt-16 pb-16">
+      <div className="w-[80%] mx-auto items-start grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-10">
+        <div>
+          <div className="text-white font-bold text-3xl">LOGO</div>
+          <p className="mt-5 font-semibold text-gray-300 text-sm">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim nihil
+            dolorum velit voluptate odit totam!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
